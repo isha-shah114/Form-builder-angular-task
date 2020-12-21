be2ffd8a65347fce822cb91365068dc70af9dfe7
isha-shah114 <isha@promactinfo.com>
2020-12-21 11:48:22 +0530

    new files added

--- src/app/add-fields/add-fields.component.ts
+++ src/app/add-fields/add-fields.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-add-fields',
@@ -9,9 +10,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AddFieldsComponent implements OnInit {
   dynamicForm: FormGroup;
   submitted = false;
-  formValue: any = {};
-  user: any = {};
-  users:any = [];
+  formValue: any = [];
   fieldName:any = 'Show';
   replacePlaceHolder: string;
   textPlaceHolder: string;
@@ -27,7 +26,22 @@ export class AddFieldsComponent implements OnInit {
   showTextarea:boolean = false;
   showSubmitButton:boolean = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  // formArray: any[] = [{
+  //   textField: '',
+  //   numberField: '',
+  //   radioButton: '',
+  //   checkBox: '',
+  //   textarea: '',
+  //   submitButton: '',
+  //   replacePlaceHolder: '',
+  // }];
+
+  formArray: any[] = [{
+    fieldType: '',
+    placeholder: '',
+  }];
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.dynamicForm = this.formBuilder.group({
@@ -36,89 +50,63 @@ export class AddFieldsComponent implements OnInit {
     });
   }
 
-  onSubmit()
-      {
-        this.user = Object.assign(this.user, this.dynamicForm.value);
-        this.addUser(this.user);
-        if(this.user.fieldType == "textField") {
-          this.showTextField = !this.showTextField;
-          this.textPlaceHolder= this.user.placeholder;
-          this.users = [this.user, ...this.users];
-          // localStorage.clear();
-          console.log(this.users);
-        } 
-      } 
-      
-      addUser(user)
-      {
-        if(localStorage.getItem('Users')){
-          this.users = JSON.parse(localStorage.getItem('Users'));
-          // if(this.user.fieldType == "textField") {
-          //     this.showTextField = !this.showTextField;
-          //     this.textPlaceHolder= this.user.placeholder;
-          //     this.users = [user, ...this.users];
-          //     console.log(this.users);
-          //   } 
-        }
-        
-        localStorage.setItem('Users', JSON.stringify(this.users));
-      }
-    }
-
-  // add() {
+  add() {
     // this.formArray = new formArray();
     // this.dataArray.push(this.formArray);
     
-   
+    this.formValue = this.dynamicForm.value;
 
-    // this.formArray.push({
-    //   fieldType: this.formValue.fieldType,
-    //   placeholder: this.formValue.placeholder,
-    //   // replacePlaceHolder: this.replacePlaceHolder,
-    // });   
-    // this.formValue = JSON.parse(localStorage.getItem('Users'));
-   
+    this.formArray.push({
+      fieldType: this.formValue.fieldType,
+      placeholder: this.formValue.placeholder,
+    });   
+    // this.userService.addUser(this.formValue);
     
-    // if(this.formValue.fieldType == "textField") {
-    //   this.showTextField = !this.showTextField;
-    //   // if(this.formArray.length <= 2){
-    //   this.textPlaceHolder= this.formValue.placeholder;
-    //   // }
-
-    //   // if(this.formArray.length > 2){
-    //     // this.replacePlaceHolder = this.formValue.placeholder;
-    //     // console.log(this.replacePlaceHolder);
-    //   // }
-    // }
-        // if(this.formValue.fieldType == "numberField") {
-        //   this.showNumberField = !this.showNumberField;
-        //   this.fieldName = "Show";
-        //   this.numberPlaceHolder= this.formValue.placeholder;
-        // }
-
-        // if(this.formValue.fieldType == "radioButton") {
-        //   this.showRadioButton = !this.showRadioButton;
-        //   this.fieldName = "Show";
-        //   this.radioButtonValue= this.formValue.placeholder;
-        //   console.log(this.radioButtonValue);
-        // }
-
-        // if(this.formValue.fieldType == "checkBox") {
-        //   this.showCheckBox = !this.showCheckBox;
-        //   this.fieldName = "Show";
-        //   this.checkBoxValue= this.formValue.placeholder;
-        //   console.log(this.checkBoxValue);
-        // }
-
-        // if(this.formValue.fieldType == "textarea") {
-        //   this.showTextarea = !this.showTextarea;
-        //   this.fieldName = "Show";
-        //   this.textareaPlaceHolder= this.formValue.placeholder;
-        // }
-
-        // if(this.formValue.fieldType == "button") {
-        //   this.showSubmitButton = !this.showSubmitButton;
-        //   this.fieldName = "Show";
-        //   this.buttonValue= this.formValue.placeholder;
-        // }
+    if(this.formValue.fieldType == "textField") {
+      // if(this.formArray.length <= 2){
+      this.textPlaceHolder= this.formValue.placeholder;
       // }
+
+      // if(this.formArray.length > 2){
+        // this.replacePlaceHolder = this.formValue.placeholder;
+        // console.log(this.replacePlaceHolder);
+      // }
+    }
+        if(this.formValue.fieldType == "numberField") {
+          this.showNumberField = !this.showNumberField;
+          this.fieldName = "Show";
+          this.numberPlaceHolder= this.formValue.placeholder;
+        }
+
+        if(this.formValue.fieldType == "radioButton") {
+          this.showRadioButton = !this.showRadioButton;
+          this.fieldName = "Show";
+          this.radioButtonValue= this.formValue.placeholder;
+          console.log(this.radioButtonValue);
+        }
+
+        if(this.formValue.fieldType == "checkBox") {
+          this.showCheckBox = !this.showCheckBox;
+          this.fieldName = "Show";
+          this.checkBoxValue= this.formValue.placeholder;
+          console.log(this.checkBoxValue);
+        }
+
+        if(this.formValue.fieldType == "textarea") {
+          this.showTextarea = !this.showTextarea;
+          this.fieldName = "Show";
+          this.textareaPlaceHolder= this.formValue.placeholder;
+        }
+
+        if(this.formValue.fieldType == "button") {
+          this.showSubmitButton = !this.showSubmitButton;
+          this.fieldName = "Show";
+          this.buttonValue= this.formValue.placeholder;
+        }
+      }
+
+      onSubmit()
+      {
+        console.log(this.formArray);
+      }    
+    }
